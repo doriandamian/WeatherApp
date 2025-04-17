086f3117fccaf851f409a27608fd4cb3381dfacf
BClaudiu02 <bogdanc89@yahoo.de>
2025-04-17 14:27:19 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WeatherDashboardComponent } from './weather-dashboard/weather-dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [WeatherDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
