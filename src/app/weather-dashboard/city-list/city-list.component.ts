b77d26c1cbff527ce31464efa63d7a749a9e1b5f
StudentClaudiu <bogdanc89@yahoo.de>
2025-08-10 17:42:31 +0300

    fixed carousel

--- src/app/weather-dashboard/city-list/city-list.component.ts
+++ src/app/weather-dashboard/city-list/city-list.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { CityCardComponent } from './city-card/city-card.component';
 import { CommonModule } from '@angular/common';
 import { CityService } from '../../shared/services/city.service';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { City } from '../../shared/models/city.model';
 import { CarouselModule } from 'primeng/carousel';
 import { FormsModule } from '@angular/forms';
@@ -17,19 +18,36 @@ import { FormsModule } from '@angular/forms';
 export class CityListComponent {
   cities$!: Observable<City[]>;
   showFavoritesOnly = false;
+  carouselKey = 0;
+  @ViewChild('scrollRow', { static: false }) scrollRow!: ElementRef<HTMLDivElement>;
+
+  scrollLeft() {
+    if (this.scrollRow && this.scrollRow.nativeElement) {
+      this.scrollRow.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+    }
+  }
+
+  scrollRight() {
+    if (this.scrollRow && this.scrollRow.nativeElement) {
+      this.scrollRow.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+    }
+  }
 
   constructor(public cityService: CityService) {
     this.onFilterChange();
   }
 
   onFilterChange() {
-    this.cities$ = this.showFavoritesOnly
+    this.cities$ = (this.showFavoritesOnly
       ? this.cityService.favoriteCities$
-      : this.cityService.cities$;
+      : this.cityService.cities$).pipe(
+        tap((cities: City[]) => { this.carouselKey = cities.length ? cities.map((c: City) => c.name).join('-').hashCode() : 0; })
+      );
   }
 
   onDelete(city: City) {
     this.cityService.removeCity(city);
+    this.carouselKey++;
   }
 
   onSetCurrent(city: City) {
@@ -38,9 +56,28 @@ export class CityListComponent {
 
   onToggleFavorite(city: City) {
     this.cityService.toggleFavorite(city);
+    this.carouselKey++;
   }
 
   getNumVisible(length: number): number {
     return Math.min(length, 5);
   }
+}
+
+declare global {
+  interface String {
+    hashCode(): number;
+  }
+}
+if (!String.prototype.hashCode) {
+  String.prototype.hashCode = function(): number {
+    let hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr   = this.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
 }
